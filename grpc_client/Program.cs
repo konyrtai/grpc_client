34ffbc2193c7ab8c4a3f16da5a39fb7fbd9d5400
Alibek Kussanov <alibek.kusanov@mail.ru>
2022-07-21 01:42:01 +0600

    add git submodule

--- grpc_client/Program.cs
+++ grpc_client/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// создаем канал для обмена сообщениями с сервером
+// параметр - адрес сервера gRPC
+
+using Grpc.Net.Client;
+using SimpleGrpcService;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5016");
+// создаем клиента
+var client = new Greeter.GreeterClient(channel);
+Console.Write("Введите имя: ");
+string name = Console.ReadLine();
+// обмениваемся сообщениями с сервером
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+Console.WriteLine("Ответ сервера: " + reply.Message);
+Console.ReadKey();
